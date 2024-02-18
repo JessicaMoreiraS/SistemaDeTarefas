06199b312afcce222e0f1c26e73d3e0f69a1ae91
Jéssica Moreira <100448388+JessicaMoreiraS@users.noreply.github.com>
2024-02-17 23:13:00 -0300

    Add files via upload

---
+++ Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data.Map;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            :base(options)
+        {
+        }
+
+        /*UsuarioModel representa uma tabela no banco de dados, essa tabela se chama Usuarios*/
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        /*Task é para que seja assincrono*/
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
+    }
+}
